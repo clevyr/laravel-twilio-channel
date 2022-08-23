eb4bd0498a04bac9dec9ab33e55e2dd7ba5a459c
aaron <alkrauss48@gmail.com>
2022-08-23 11:40:01 -0500

    Add tests for TwilioMessage

---
+++ tests/TwilioMessageTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use Clevyr\LaravelTwilioChannel\Messages\TwilioMessage;
+
+beforeEach(function () {
+    $this->message = new TwilioMessage();
+
+    $this->message
+        ->line('Line 1')
+        ->line('Line 2');
+});
+
+it('can add lines', function () {
+    expect(count($this->message->message))->toBe(2);
+    expect($this->message->message[1])->toBe('Line 2');
+});
+
+it('prints the full message', function () {
+    expect($this->message->fullMessage())->toBe("Line 1\n\nLine 2");
+});
